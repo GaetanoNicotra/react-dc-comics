3a20b0244247417bdc8ae9da7de05bb583d23b60
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-05 16:43:22 +0200

    [UPD] stilizzazione del main top

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 // Importo l'Header
 import Header from "./components/header";
+// Importo il Main
+import Main from "./components/Main";
+
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+// definisco il Main
+
+const Main = () => {
+    return (
+        <div className="container">
+            <div className="main-top">
+                --Content goes here
+            </div>
+            <div className="main-bottom"></div>
+            <ul>
+                <li><img src="../img/buy-comics-digital-comics.png" alt="buy" /></li>
+            </ul>
+        </div>
+    )
+};
+
+export default Main;
