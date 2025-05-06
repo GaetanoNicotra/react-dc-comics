2833dffdafd74ae72ffacf44a80e313ecc9ffaab
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-06 15:42:21 +0200

    [UPD] creazione della prima card fumetto

--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -2,9 +2,15 @@
 function ListComics() {
     return (
         <section>
-            <h2>MESSAGGIO DI PROVA</h2>
+            <h4>CURRENT SERIES</h4>
+            <div className="container card-container">
+                <div className="card-fumetto">
+                    <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" alt="SUPERMAN" />
+                    <h6>ACTION COMICS</h6>
+                </div>
+            </div>
         </section>
     )
-}
+};
 
-export default ListComics
+export default ListComics;
