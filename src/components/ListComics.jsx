95de96d9433332dbebf4bcaae311f6c5e259cf84
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-06 19:31:04 +0200

    [UPD] aggiunto rettangolo blu nel top main

--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -159,7 +159,7 @@ function ListComics() {
     return (
 
         <section>
-            <div>CURRENT SERIES</div>
+            <div className="rettangolo-blue">CURRENT SERIES</div>
             <div className="card-container">
                 <ComcicCard src={comics[0].thumb} title={comics[0].series} />
                 <ComcicCard src={comics[1].thumb} title={comics[1].series} />
